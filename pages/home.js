858efdc5f71ea8f5b66c5522a5aec3b494836aad
maymhuu <maymyatmon@ayainnovation.com>
2022-11-13 23:59:48 +0630

    chnaged ui in home

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import client from "../apolloClient";
 import { gql } from "@apollo/client";
 import Image from "next/image";
 
-export default function Home({ menus,products }) {
+export default function Home({ menus, products }) {
   const { jsonField, content, image } = menus[0];
 
   const findData = (key) => {
@@ -19,7 +19,7 @@ export default function Home({ menus,products }) {
   };
 
   return (
-    <div className="container mx-auto sm:px-0 overflow-hidden relative w-4/5 text-babyBrown">
+    <div className="container mx-auto sm:px-0 overflow-hidden relative w-4/5 text-amber-800">
       <Image
         alt="main"
         src={image[0].url}
@@ -30,39 +30,40 @@ export default function Home({ menus,products }) {
       />
       <div className=" w-full h-full flex-col text-center ">
         <div
-          className=" pt-8 pb-9 text-babyBrown"
+          className=" pt-8 pb-9 "
           dangerouslySetInnerHTML={{ __html: content.html }}
         ></div>
 
-        <h2 className="text-babyBrown font-extrabold pb-7">Our Core Values</h2>
+        <h2 className=" font-extrabold pb-7">Our Core Values</h2>
       </div>
 
       <div className=" w-full h-full flex flex-row bg-slate-400 justify-center text-center">
-        <div className="w-1/3 p-12  bg-yellow-50 ">{findData("first")}</div>
+        <div className="w-1/3 p-12 bg-yellow-50">{findData("first")}</div>
         <div className="w-1/3 p-12 bg-stone-200">{findData("second")}</div>
         <div className="w-1/3 p-12 bg-orange-50">{findData("third")}</div>
       </div>
-
-      <div className="animate-carouselAnim carousel-items flex items-center justify-center w-fit">
+      <h2 className=" font-extrabold pt-6 text-center">Menus</h2> 
+      <div className="animate-carouselAnim carousel-items flex  items-center justify-center w-fit">
         {products &&
           products.length > 0 &&
           products.map((data, index) => {
             return (
-              <div key={index}
-                className="carousel-focus flex items-center flex-col relative bg-lightBrown mx-5 my-10 px-4 py-3 rounded-lg shadow-lg"
-                style={{ width: "270px" }}
+              <div
+                key={index}
+                className="h-56 w-64 carousel-focus flex items-center flex-col relative bg-lightBrown mx-5 my-10 px-4 py-3 rounded-lg shadow-lg"
               >
-                <span className="text-babyBrown font-bold text-xl mb-3">
-                  {data.name}
-                </span>
                 <img
-                  className="h-16 w-16 rounded-full shadow-2xl"
+                  className=" mt-3 h-16 w-16 rounded-full shadow-2xl"
                   src={data.image.url}
                   alt="Img"
                 />
-                <p className="mt-3 text-gray-600 text-center">
-                  {data.name}
-                </p>
+                <p className="mt-3 font-semibold text-amber-800 text-center">{data.name}</p>
+                <div
+                  className="article-style text-left text-amber-800"
+                  dangerouslySetInnerHTML={{
+                    __html: data.productDescription?.html ?? "",
+                  }}
+                ></div>
               </div>
             );
           })}
@@ -93,7 +94,9 @@ export async function getStaticProps() {
           slug
           date
           name
-          description
+          productDescription {
+            html
+          }
           price
           image {
             url
